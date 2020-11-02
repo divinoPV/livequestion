ed37343f5bef37bed75360d52a04f584767e474e
Hugo <monteiro.hugo2001@icloud.com>
2020-11-02 17:09:46 +0100

    refactor(encoder): password encoder

--- src/DataFixtures/UserFixtures.php
+++ src/DataFixtures/UserFixtures.php
@@ -8,9 +8,17 @@ use Doctrine\Persistence\ObjectManager;
 use Faker\Factory;
 use Faker\Provider\Image;
 use Faker\Provider\Lorem;
+use Symfony\Component\Security\Core\Encoder\UserPasswordEncoderInterface;
 
 class UserFixtures extends Fixture
 {
+    private UserPasswordEncoderInterface $passwordEncoder;
+
+    public function __construct(UserPasswordEncoderInterface $passwordEncoder)
+    {
+        $this->passwordEncoder = $passwordEncoder;
+    }
+
     public function load(ObjectManager $manager)
     {
         for ($i = 1; $i <= 24; $i++) {
@@ -28,9 +36,13 @@ class UserFixtures extends Fixture
                 ->addRole($role[$key])
                 ->addGender($gender[$key])
                 ->setEmail($username."@outlook.com")
+                ->setUsername($username)
                 ->setImage(Image::imageUrl($width = 640, $height = 480))
-                ->setPlainPassword($password)
-                ->setUsername($username);
+                ->setPlainPassword($password);
+
+            $password = $this->passwordEncoder->encodePassword($user, $user->getPlainPassword());
+            $user->setPassword($password);
+
             var_dump($user->getPlainPassword());
 
             $manager->persist($user);
