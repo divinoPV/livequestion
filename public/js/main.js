c79794de171a54b2df2a4e3aa7a63658b4f4ae5d
Hugo <monteiro.hugo2001@icloud.com>
2020-11-11 18:32:38 +0100

    build(home): import public files, update twig base, create nav and footer

---
+++ public/js/main.js
@@ -0,0 +1,87 @@
+//Button Link Home Page
+$(function(){
+	$("#btn-link-1").click(function(){
+		$('#hp-lien-0').show();
+		$('#hp-lien-1').hide();
+		$('#hp-lien-2').hide();
+		$('#btn-link-1').css({
+			"color": "white",
+			"background-color": "#e10080",
+		});
+		$('#btn-link-2').css({
+			"color": "#e10080",
+			"background-color": "#f8f9fd",
+		});
+		$('#btn-link-3').css({
+			"color": "#e10080",
+			"background-color": "#f8f9fd",
+		});
+	});
+	$("#btn-link-2").click(function(){
+		$('#hp-lien-0').hide();
+		$('#hp-lien-1').show();
+		$('#hp-lien-2').hide();
+		$('#btn-link-1').css({
+			"color": "#e10080",
+			"background-color": "#f8f9fd",
+		});
+		$('#btn-link-2').css({
+			"color": "white",
+			"background-color": "#e10080",
+		});
+		$('#btn-link-3').css({
+			"color": "#e10080",
+			"background-color": "#f8f9fd",
+		});
+	});
+	$("#btn-link-3").click(function(){
+		$('#hp-lien-0').hide();
+		$('#hp-lien-1').hide();
+		$('#hp-lien-2').show();
+		$('#btn-link-1').css({
+			"color": "#e10080",
+			"background-color": "#f8f9fd",
+		});
+		$('#btn-link-2').css({
+			"color": "#e10080",
+			"background-color": "#f8f9fd",
+		});
+		$('#btn-link-3').css({
+			"color": "white",
+			"background-color": "#e10080",
+		});
+	});
+});
+//Login Register
+$(function(){
+	$("#btn-login").click(function(){
+		$('#login-display').show();
+		$('#register-display').hide();
+		$('#btn-login').css({
+			"color": "white",
+			"background-color": "#e10080",
+		});
+		$('#btn-registre').css({
+			"color": "#e10080",
+			"background-color": "#f8f9fd",
+		});
+	});
+	$("#btn-registre").click(function(){
+		$('#login-display').hide();
+		$('#register-display').show();
+		$('#btn-login').css({
+			"color": "#e10080",
+			"background-color": "#f8f9fd",
+		});
+		$('#btn-registre').css({
+			"color": "white",
+			"background-color": "#e10080",
+		});
+	});
+});
+//Button Search
+$(function search_pcl_btn(){
+$("#search-pcl-btn").click(function search_pcl_btn(){
+	$('#search-pcl-filtre').toggle();
+	});
+});
