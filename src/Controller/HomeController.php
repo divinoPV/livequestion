e8eec73ca2ca16c18d3163bf5fb8d2b5b00e8427
Hugo <monteiro.hugo2001@icloud.com>
2020-11-11 19:03:26 +0100

    refactor(friend): increase HomeController dispatch two routes, add route addFriend

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -12,6 +12,7 @@ use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\HttpFoundation\Session\Session;
 use Symfony\Component\Routing\Annotation\Route;
 
 final class HomeController extends AbstractController
@@ -22,36 +23,47 @@ final class HomeController extends AbstractController
      * @param QuestionService $questionService
      * @param AnswerService $answerService
      * @param CategoryService $categoryService
-     * @param Friend $friend
-     * @param EntityManagerInterface $manager
-     * @param Request $request
+     * @param Session $session
      * @return Response
      */
     public function index(UserService $profilService,
                           QuestionService $questionService,
                           AnswerService  $answerService,
                           CategoryService $categoryService,
-                          Friend $friend,
-                          EntityManagerInterface $manager,
-                          Request $request): Response
+                          Session $session): Response
     {
-        $addFriend = '';
-
-        if ($request->getMethod() == 'POST') {
-            $targetUser = $manager->getRepository(User::class)
-                ->find($request->request->get("receiver"));
-            $connectedUser = $manager->getRepository(User::class)
-                ->find($request->request->get("sender"));
-
-            $addFriend = $friend->addFriend($targetUser, $connectedUser);
-        }
 
         return $this->render('home/index.html.twig', [
             'profils' => $profilService->getFullProfil(),
             'questions' => $questionService->getFullQuestion(),
             'answers' => $answerService->getFullAnswer(),
             'categories' => $categoryService->getFullCategory(),
-            'addFriend' => $addFriend
+            'message' => $session->get('message')
         ]);
+    }
+
+    /**
+     * @Route("/addFriend", name="addFriend")
+     * @param Friend $friend
+     * @param EntityManagerInterface $manager
+     * @param Request $request
+     * @param Session $session
+     * @return Response
+     */
+    public function addFriend(Friend $friend,
+                              EntityManagerInterface $manager,
+                              Request $request,
+                              Session $session): Response
+    {
+        if ($request->getMethod() == 'POST') {
+            $targetUser = $manager->getRepository(User::class)
+                ->find($request->request->get("receiver"));
+            $connectedUser = $manager->getRepository(User::class)
+                ->find($request->request->get("sender"));
+
+            $session->set('message', $friend->addFriend($targetUser, $connectedUser));
+        }
+
+        return $this->redirect($this->generateUrl('home'));
     }
 }
